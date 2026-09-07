cd4e19788c03ffe268c18f8bf89b0aa17336f06d
darthamat <darthamat@gmail.com>
2026-09-07 14:06:22 +0200

    1406

--- js/perfil.js
+++ js/perfil.js
@@ -1,6 +1,6 @@
 import { getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { app } from "js/firebase-config.js";
+import { app } from "./firebase-config.js";
 import { generarResumenEvolucion } from "js/sintetizadorPerfil.js";
 
 const auth = getAuth(app);
